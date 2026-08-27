be4698416dc8b48c44289302af9c5e8e0144846b
Johnny <johnnylbraganca@gmail.com>
2026-08-27 00:04:47 -0300

    adicionei opção de escrever o que deseja salvar

--- projeto.c
+++ projeto.c
@@ -2,16 +2,19 @@
 
 int main(){
     int entrada;
+    char escrita[100];
     FILE *save = fopen("dados.txt", "w");
     if (save == NULL) {
         printf("erro ao abrir o programa\n");
         return 1;
     }
-    fprintf(save, "Primeiro teste de escrita em C");
+    printf("digite o que deseja salvar no arquivo\n");
+    fgets(escrita, sizeof(escrita), stdin);
+    fprintf(save, "%s", escrita);
     fclose(save);
     printf("arquivo gravado e fechado com sucesso\n");
 
-    printf("digite 1 para ler o arquivo\n");
+    printf("digite 1 para ler o arquivo, 0 para parar o codigo\n");
     scanf("%d", &entrada);
 
     switch (entrada)
@@ -25,7 +28,8 @@ int main(){
         fclose(ler);
         printf("lido: %s", conteudo);
         break;
-    
+    case 2:
+    return 0;
     default:
         break;
     }
